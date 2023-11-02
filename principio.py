284ab21e4cd448949a01eb40f652564dd916ceb9
sebas <sebastianqjuarez@gmail.com>
2023-11-01 19:07:00 -0600

    se pueden mandar los transportes para que el chofer los arranque todos

--- principio.py
+++ principio.py
@@ -24,10 +24,19 @@ class Avion(Transporte):
     def transportar(self):
         print(f"Soy el avión y transporto {self._pasajeros} pasajeros")
 
+class Chofer():
+    def manejar(self,transporte: Transporte):
+        print("Soy el chofer y voy a arrancar el transporte")
+        transporte.transportar()
 
 if __name__ == "__main__":
     auto_chevy = Auto(pasajeros = 4)
-    auto_chevy.transportar()
+    #auto_chevy.transportar()
 
     auto_winstar = Auto(pasajeros = 8)
-    auto_winstar.transportar()
+    #auto_winstar.transportar()
+    
+    mis_transportes = [auto_chevy, auto_winstar]
+    chofer = Chofer()
+    for transporte in mis_transportes:
+        chofer.manejar(transporte)
