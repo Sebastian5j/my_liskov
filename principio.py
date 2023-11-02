f332944da1a6be4cf0e8e62c8a9eea23214ab283
sebas <sebastianqjuarez@gmail.com>
2023-11-01 19:00:41 -0600

    primeras dos instancias correjas de doy autos con diferentes pasajeros

---
+++ principio.py
@@ -0,0 +1,33 @@
+
+from abc import ABCMeta, abstractmethod
+
+class Transporte(metaclass=ABCMeta):
+    @abstractmethod
+    def __init__(self, **kwargs):
+        self._pasajeros = kwargs["pasajeros"]
+
+    def transportar(self):
+        pass
+
+class Auto(Transporte):
+    def __init__(self, **kwargs) -> None:
+        super().__init__(**kwargs)
+    def transportar(self):
+        print(f"Soy el auto y transporto {self._pasajeros} pasajeros") 
+ 
+class Moto(Transporte):
+    def transportar(self):
+        print(f"Soy la moto y transporto {self._pasajeros} pasajeros")
+
+
+class Avion(Transporte):
+    def transportar(self):
+        print(f"Soy el avión y transporto {self._pasajeros} pasajeros")
+
+
+if __name__ == "__main__":
+    auto_chevy = Auto(pasajeros = 4)
+    auto_chevy.transportar()
+
+    auto_winstar = Auto(pasajeros = 8)
+    auto_winstar.transportar()
